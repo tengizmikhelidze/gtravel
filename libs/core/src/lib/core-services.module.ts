a4a1815d1cb62f19f999cc184cdf20bbe91bf16a
tengizmikhelidze <71060352+tengizmikhelidze@users.noreply.github.com>
2021-10-18 13:57:38 +0000

    Deploying to gh-pages from @ tengizmikhelidze/G-travel@499e6a2f0e18ee0c242a66c09f3efa6c80be39a1 🚀

--- libs/core/src/lib/core-services.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { EnvironmentManagerModule } from './environment-manager/environment-manager.module';
-
-@NgModule({
-  imports: [
-    EnvironmentManagerModule
-  ],
-})
-export class CoreServicesModule {}
--- libs/shared/forms/src/lib/entity/control-types.ts
+++
@@ -1,3 +0,0 @@
-export type ControlValueTypes = string | number | Date | boolean | undefined;
-export type ControlTypes = 'textBox' | 'numeric' | undefined;
-export type PatterTypes = 'eng' | 'email'
