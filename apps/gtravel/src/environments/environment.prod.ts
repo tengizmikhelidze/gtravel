2e8df9b6562a30084577132655a1b25ad6e596c1
tengizmikhelidze <71060352+tengizmikhelidze@users.noreply.github.com>
2021-10-18 13:25:43 +0000

    Deploying to gh-pages from @ tengizmikhelidze/G-travel@8b85a27a732fd6b8e93fa8c537ef0ec6a10275dd 🚀

--- apps/gtravel/src/environments/environment.prod.ts
+++ apps/gtravel/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 import { MergedEnvironment } from '../../../../libs/core/src/lib/environment-manager/entity/environment';
 
 export const environment: MergedEnvironment = {
+  baseHref: '',
   production: true,
 };
